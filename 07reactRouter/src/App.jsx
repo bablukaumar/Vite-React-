31ad938b225697a73a44af254b52ec979b706d3f
bablu Kumar <bablu824130@gmail.com>
2024-08-21 01:05:28 +0530

    Added all recovery file

---
+++ 07reactRouter/src/App.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import Header from '../src/components/Header/Header'
+import Home from '../src/components/Home/Home'
+import Footer from '../src/components/Footer/Footer'
+
+function App() {
+  return (
+    <>
+      <Header/>
+      <Home/>
+      <Footer/>
+    </>
+  )
+}
+
+export default App
